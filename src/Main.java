2d5e230ba16c0d83f9cba90ff3c489c169c0aa9b
unknown <future870914@gmail.com>
2017-11-13 15:54:57 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in=new Scanner(System.in);
+	    int i;
+        for(i=1;i<=7;i++)
+	        System.out.println(i);
+
+
+    }
+}
